56a769c5801c57d75a72a8d9915bc10cb394663c
chimichi004 <chember004@gmail.com>
2023-05-16 16:56:26 +0800

    initial traversy-media angular-11 tuts beginners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-proj-2023';
+  title: string = 'Task Tracker';
 }
